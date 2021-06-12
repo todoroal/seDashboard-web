db1d4f413566b3838bdc66bd5478db096c4a23c6
csdc23bb-thomas-gutjahr <71706791+csdc23bb-thomas-gutjahr@users.noreply.github.com>
2021-06-12 20:53:28 +0200

    Integrated RSS Feed

---
+++ frontend/src/components/standardrss/StandardRss.js
@@ -0,0 +1,54 @@
+import React, { Fragment} from 'react';
+
+const StandardRss = () => {
+
+    const tmpData = {
+    
+      "items": 
+     [
+      {
+        "title": "Anzeigen, Ermittlungen, Chats: Für die ÖVP wird die Luft dünn",
+        "link": "https://www.derstandard.at/story/2000127335201/anzeigen-ermittlungen-chats-fuer-die-oevp-wird-die-luft-duenn?ref=rss",
+        "thumbnail": "https://images.derstandard.at/img/2021/06/11/APA-schlagerr.jpg?w=150&s=cfd51598",
+      },
+      {
+        "title": "Test",
+        "link": "https://www.derstandard.at/story/2000127335201/anzeigen-ermittlungen-chats-fuer-die-oevp-wird-die-luft-duenn?ref=rss",
+        "thumbnail": "https://images.derstandard.at/img/2021/06/11/APA-schlagerr.jpg?w=150&s=cfd51598",
+      }
+     ]
+    }
+
+
+    
+
+    console.log(tmpData, 'tmp data')
+
+    
+
+const {items} = tmpData  
+
+
+console.log(items.title, 'title')
+//console.log(weather.icon, 'weather')  
+
+ return (
+    <Fragment>
+      <div className="container-dash">
+        <h2>derStandard Newsroom</h2>
+        <div className="image-wrapper">
+        {items.map(item =>{
+            <div>
+            <a href={`${item.link}`}>{item.title}</a>  
+            <img src={`${item.thumbnail}`}  alt="derStandard" width="100%" height="100%"/>
+            </div>
+            console.log(item.title, 'ITEM')
+        })}          
+        </div>       
+      </div>
+    </Fragment>
+
+)
+};
+
+export default StandardRss;

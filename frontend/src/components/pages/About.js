d11b61867878f001ed2c1d595dfc7217dc869050
csdc23bb-thomas-gutjahr <71706791+csdc23bb-thomas-gutjahr@users.noreply.github.com>
2021-06-14 19:49:26 +0200

    About us Section improvements

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -7,27 +7,27 @@ import React from 'react'
             <img src={`https://safe.hbox.at/index.php/apps/files_sharing/publicpreview/PNbxziMMnMmAiB4?x=2560&y=947&a=true&file=tl.png`} align="left" alt="Thomas L" width="148px" height="144px"/>
                 {/* <p>Thomas Lerchbaumer [Frontend mastermind, Backend general support] Why JavaDevs wear glasses? Because they don’t C#.</p> */}
                 <ul className="item-aboutus-ul">
-                    <li>Thomas Lerchbaumer</li>
+                    <li className=".item-aboutus-li-bold">Thomas Lerchbaumer</li>
                     <li>[Frontend mastermind, Backend general support]</li>
-                    <li>Why JavaDevs wear glasses? Because they don’t C#.</li>
+                    <li className="item-aboutus-li-italic">Why JavaDevs wear glasses? Because they don’t C#.</li>
                 </ul>
             </div>
             <div class="item-aboutus">
             <img src={`https://safe.hbox.at/index.php/apps/files_sharing/publicpreview/sqeCBcedM9XDdsg?x=1920&y=587&a=true&file=dd.png`} align="left" alt="Dominic D" width="145px" height="137px"/>
                 {/* <p>Dominic Duda [Main Logic, Backend mastermind] Why are Assembly programmers always soaking wet? They work below C-level.</p> */}
                 <ul className="item-aboutus-ul">
-                    <li>Dominic Duda</li>
+                    <li className=".item-aboutus-li-bold">Dominic Duda</li>
                     <li>[Main Logic, Backend mastermind]</li>
-                    <li>Why are Assembly programmers always soaking wet? They work below C-level.</li>
+                    <li className="item-aboutus-li-italic">Why are Assembly programmers always soaking wet? They work below C-level.</li>
                 </ul>
             </div>
             <div class="item-aboutus">
             <img src={`https://safe.hbox.at/index.php/apps/files_sharing/publicpreview/74xmTNNxfmXrgif?x=1920&y=587&a=true&file=tg.png`} align="left" alt="Thomas G" width="153px" height="145px"/>
                 {/* <p>Thomas Gutjahr [] </p> */}
                 <ul className="item-aboutus-ul">
-                    <li>Thomas Gutjahr</li>
+                    <li className=".item-aboutus-li-bold">Thomas Gutjahr</li>
                     <li>[Frontend support, Backend support]</li>
-                    <li>Don't use "beef stew" as a computer password. It's not stroganoff.</li>
+                    <li className="item-aboutus-li-italic">Don't use "beef stew" as a computer password. It's not stroganoff.</li>
                 </ul>
             </div>
         </div>

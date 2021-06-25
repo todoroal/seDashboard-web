c27c6db2b455a5bf4c30514bb150cd9a29b77c28
Thomas <thomas.lerchbaumer@stud.fh-campuswien.ac.at>
2021-06-25 20:02:48 +0200

    fixed parsing problem, added favicon

--- frontend/src/context/news/newsReducer.js
+++ frontend/src/context/news/newsReducer.js
@@ -2,19 +2,19 @@ import {
     GET_NEWS
 } from '../types'
 
-export default (state, action) =>{
-    switch(action.type){
-       
-               case GET_NEWS:
-                   return{
-                       ...state,
-                       news: action.payload,
-                       loading: false
-                   }
-               default:
-                   return state;
-       
-           }
-       
-       
-       }
+export default (state, action) => {
+    switch (action.type) {
+
+        case GET_NEWS:
+            return {
+                ...state,
+                news: action.payload,
+                loading: action.payload.length > 0 ? false : true
+            }
+        default:
+            return state;
+
+    }
+
+
+}
